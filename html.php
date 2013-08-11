69b42789b3eb272f5aa5b2d25c335311871a32c3
pdostal <git@pdostal.cz>
2013-08-11 07:13:35 +0200

    HTTP auth & MySQL connect

--- html.php
+++ html.php
@@ -1,3 +1,8 @@
+<?php
+	$pass = trim(htmlspecialchars(htmlspecialchars_decode($_POST['pass'], ENT_NOQUOTES), ENT_NOQUOTES));
+
+	if ( empty($pass) && empty($_SESSION['login']) ) {
+?>
 <!doctype html>
 <html lang="en">
 	<head>
@@ -7,5 +12,45 @@
 	</head>
 	<body>
 		<h1>twitter-robot</h1>
+		<form method="post">
+			<label for="pass">Password: </label>
+			<input type="password" id="pass" name="pass" size="12" maxlength="12" ></input>
+			<input type="submit" value="Login"></input></span>
+		</form>
 	</body>
 </html>
+<?php
+	}
+	if ( !empty($pass) ) {
+		if ( md5($pass) == $config['htpass'] ) { $_SESSION['login'] = 'logged'; }
+		header("HTTP/1.1 301 Moved Permanently");
+		header("Location: index.php");
+		header("Connection: close");
+		exit();
+	}
+	if ( !empty($_SESSION['login']) ) {
+?>
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="robots" content="noindex">
+		<title>twitter-robot</title>
+	</head>
+	<body>
+		<h1>twitter-robot</h1>
+		<?php
+			$url = 'https://api.twitter.com/1.1/statuses/mentions_timeline.json';
+			$getfield = '?';
+			$requestMethod = 'GET';
+			$twitter = new TwitterAPIExchange($settings);
+			echo $twitter->setGetfield($getfield)
+			             ->buildOauth($url, $requestMethod)
+			             ->performRequest();
+			echo "xexexe";
+		?>
+	</body>
+</html>
+<?php
+	}
+?>
--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <?php
+    ob_start();
+    chdir(dirname($_SERVER['SCRIPT_FILENAME']));
+    session_start();
+
     $type = trim(htmlspecialchars(htmlspecialchars_decode($_GET['type'], ENT_NOQUOTES), ENT_NOQUOTES));
-    if ($type == 'cron' || $type == 'debug') {
-        require_once('twitter.php');
+
+    if ( $type == 'cron' || $type == 'debug' ) {
         include('config.php');
+
+        if ( !empty($config['server']) && !empty($config['username']) && !empty($config['password']) && !empty($config['database']) ) {
+            $mysqli = new mysqli($config['server'], $config['username'], $config['password'], $config['database']);
+        }
+
+        require_once('twitter.php');
         $twitter = new TwitterAPIExchange($config);
-        if ($type == 'debug') {
+
+        if ( $type == 'debug' ) {
             include('html.php');
+        }
+        if ( $type == 'cron' ) {
+            include('cron.php');
         }
     } else {
         header("HTTP/1.1 301 Moved Permanently");
